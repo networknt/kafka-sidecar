cc065e7b153cdae2c60a69c2b3d52a4aeb1d87a5
Debashisa Mohanty <debashisa.mohanty@sunlife.com>
2023-02-13 12:43:38 -0500

    refactored active consumer

--- src/main/java/com/networknt/mesh/kafka/handler/ConsumersGroupInstancesInstanceRecordsGetHandler.java
+++ src/main/java/com/networknt/mesh/kafka/handler/ConsumersGroupInstancesInstanceRecordsGetHandler.java
@@ -35,29 +35,50 @@ public class ConsumersGroupInstancesInstanceRecordsGetHandler implements LightHt
         String group = exchange.getPathParameters().get("group").getFirst();
         String instance = exchange.getPathParameters().get("instance").getFirst();
         Deque<String> dequeTimeout = exchange.getQueryParameters().get("timeout");
+        Deque<String> dequeMaxBytes = exchange.getQueryParameters().get("maxBytes");
+
+        try{
+            String result = activeReadRecordUtil(group,instance,dequeTimeout, dequeMaxBytes);
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+            exchange.setStatusCode(200);
+            exchange.getResponseSender().send(result);
+        }
+        catch(FrameworkException e){
+            if(logger.isDebugEnabled()) logger.debug("FrameworkException: ", e);
+            setExchangeStatus(exchange, e.getStatus());
+        }
+    }
+
+
+    public String activeReadRecordUtil(String group,String instance, Deque<String> dequeTimeout ,Deque<String> dequeMaxBytes){
+
         long timeoutMs = -1;
         if(dequeTimeout != null) {
             timeoutMs = Long.valueOf(dequeTimeout.getFirst());
         }
-        Deque<String> dequeMaxBytes = exchange.getQueryParameters().get("maxBytes");
+
         long maxBytes = -1;
         if(dequeMaxBytes != null) {
             maxBytes = Long.valueOf(dequeMaxBytes.getFirst());
         }
         // String format = exchange.getQueryParameters().get("format").getFirst();
         // TODO find a way to overwrite the default configuration for the keyFormat and valueFormat from the query parameters.
-        readRecords(
-                exchange,
-                group,
-                instance,
-                Duration.ofMillis(timeoutMs),
-                maxBytes,
-                KafkaConsumerState.class,
-                SidecarConsumerRecord::fromConsumerRecord);
+        try {
+            return readRecords(
+                    group,
+                    instance,
+                    Duration.ofMillis(timeoutMs),
+                    maxBytes,
+                    KafkaConsumerState.class,
+                    SidecarConsumerRecord::fromConsumerRecord);
+        }
+        catch(FrameworkException e){
+            throw new FrameworkException(e.getStatus());
+        }
+
     }
 
-    private <KafkaKeyT, KafkaValueT, ClientKeyT, ClientValueT> void readRecords(
-            HttpServerExchange exchange,
+    private <KafkaKeyT, KafkaValueT, ClientKeyT, ClientValueT> String readRecords(
             String group,
             String instance,
             Duration timeout,
@@ -67,6 +88,7 @@ public class ConsumersGroupInstancesInstanceRecordsGetHandler implements LightHt
             Function<ConsumerRecord<ClientKeyT, ClientValueT>, ?> toJsonWrapper
     ) {
         maxBytes = (maxBytes <= 0) ? Long.MAX_VALUE : maxBytes;
+        final String[] result = {null};
         ActiveConsumerStartupHook.kafkaConsumerManager.readRecords(
                 group, instance, consumerStateType, timeout, maxBytes,
                 new ConsumerReadCallback<ClientKeyT, ClientValueT>() {
@@ -75,16 +97,15 @@ public class ConsumersGroupInstancesInstanceRecordsGetHandler implements LightHt
                             List<ConsumerRecord<ClientKeyT, ClientValueT>> records, FrameworkException e
                     ) {
                         if (e != null) {
-                            if(logger.isDebugEnabled()) logger.debug("FrameworkException:", e);
-                            setExchangeStatus(exchange, e.getStatus());
+                            throw new FrameworkException( e.getStatus());
                         } else {
-                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-                            exchange.setStatusCode(200);
-                            exchange.getResponseSender().send(JsonMapper.toJson(records.stream().map(toJsonWrapper).collect(Collectors.toList())));
+                            result[0] = JsonMapper.toJson(records.stream().map(toJsonWrapper).collect(Collectors.toList()));
                         }
                     }
                 }
         );
+
+        return result[0];
     }
 
 }
